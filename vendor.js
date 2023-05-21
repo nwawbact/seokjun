1177a7d734c5de99cefcfb12898948d0328c5d16
nwawbact <134189611+nwawbact@users.noreply.github.com>
2023-05-21 21:47:22 +0900

    Delete vendor.js

--- vendor.js
+++
@@ -1,8 +0,0 @@
-const hsjBtn = document.getElementById('hsj');
-const userName = document.getElementById('username');
-const greetName = document.getElementById('greeting');
-
-
-hsjBtn.addEventListener('click', hsjInformation);
-
-greetName.innerHTML = `<p>환영합니다, <strong>${userInput}</p>`
