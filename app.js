2a663696ba80afe51f68da70a3996d6cca9e9e34
nwawbact <134189611+nwawbact@users.noreply.github.com>
2023-06-06 01:03:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,35 @@ let passwordConfirmation;
 const password = '12345678';
 let masterConfirmation;
 
+const hsjBtn = document.getElementById('hsj');
+const userName = document.getElementById('username');
+const greetName = document.getElementById('greeting');
+const calcBtn = document.getElementById('calc');
+const game2048Btn = document.getElementById('2048');
+
+game2048Btn.addEventListener('click', game2048);
+hsjBtn.addEventListener('click', hsjInformation);
+
+calcBtn.addEventListener('click', calculater);
+
+function calculater() {
+    window.open("./calc.html" , "계산기");
+}
+
+function hsjInformation() {
+    window.open("./popup.html", '황석준팝업', 'width=800, height=800');
+}
+
+function game2048(){
+    window.open("./2048.html");
+}
+
 alert("hello");
 
 
 userInput = prompt("당신의 이름은? : ");
+
+
 
 if (userInput == ''){
     userInput = 'guest';
@@ -27,15 +52,8 @@ else{
     alert("대신 더 멋진 걸 보여드리겠습니다.");
 }
 
+greetName.innerHTML = `<p>환영합니다, <strong>${userInput}</p>`
 
 
 
-
-function hsjInformation() {
-    window.open("./popup.html", '황석준팝업', 'width=800, height=800');
-}
-
-function game2048(){
-    window.open("./2048.html");
-}
 
